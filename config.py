751c09cbd4e0390af1a751fae37aa74b340cf3b3
Abhijith NT <abhijithnt64@gmail.com>
2020-11-09 22:56:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class TeleInfo():
-    API_ID = int(os.environ["API_ID"])    # from https://my.telegram.org/app
-    API_HASH = os.environ["API_HASH"]     # from https://my.telegram.org/app
-    BOT_TOKEN = os.environ["BOT_TOKEN"]    # from @botfather
+    API_ID = int(os.environ.get("APP_ID", 12345))        # from https://my.telegram.org/app
+    API_HASH = os.environ.get("API_HASH", None)          # from https://my.telegram.org/app
+    BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", None)     # from @botfather
