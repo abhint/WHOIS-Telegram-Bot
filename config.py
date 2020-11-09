35821cfc79e5f76ee94751d72b5f3d9f26f7b648
Abhijith NT <abhijithnt64@gmail.com>
2020-11-09 22:18:52 +0530

    😅

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class TeleInfo():
-    API_ID =  1978879 #int(os.environ["API_ID"])    # from https://my.telegram.org/app
-    API_HASH = "2cac64c780aa3997a6d1b6ec7e248ead" #os.environ["API_HASH"]     # from https://my.telegram.org/app
-    BOT_TOKEN = "1351501215:AAH7Qg2PbVCnVsNvhniwd85HRtspSe4fDUM"   # from @botfather
+    API_ID = int(os.environ["API_ID"])    # from https://my.telegram.org/app
+    API_HASH = os.environ["API_HASH"]     # from https://my.telegram.org/app
+    BOT_TOKEN = os.environ["BOT_TOKEN"]    # from @botfather
